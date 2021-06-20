c3b50e1da2418b7ba0579e781b1a39ef083749e9
Oleg Vlasenko <vop@unity.net>
2021-06-20 15:16:59 +0300

    Initisl upload

---
+++ README.md
@@ -0,0 +1,3 @@
+### TerraForm module for Hetznet DNS
+
+Adds SSH public key to Hetzner Cloud profile for file or string
---
+++ main.tf
@@ -0,0 +1,30 @@
+########################################################################
+###
+### terraform-hetzner-webserver script
+### 2021 (c) Oleg Vlasenko, MIT License
+###
+########################################################################
+########################################################################
+### terraform requires declaration of required providers in each module
+###
+terraform {
+  required_providers {
+    hcloud = {
+           source  = "hetznercloud/hcloud"
+    }
+   }
+}
+
+
+
+## Install new ssh public key at Hetzner Cloud Console
+resource hcloud_ssh_key "ssh_key" {
+    name       = var.hcloud_ssh_public_key_name
+    public_key = var.hcloud_ssh_public_key != null ? var.hcloud_ssh_public_key : file(var.hcloud_ssh_public_key_file)
+}
+
+## Get existing ssh public key from the Hetzner Project
+# data "hcloud_ssh_key" "ssh_key" {
+#   name = var.hcloud_ssh_public_key_name
+# }
+
